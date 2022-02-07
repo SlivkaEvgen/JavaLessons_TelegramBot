16be42f4fe9d4a16ce705007b842940c130a1e4d
Java Developer <79520449+SlivkaEvgen@users.noreply.github.com>
2022-02-07 04:45:48 +0200

    Update TelegramConstants.java

--- src/main/java/org/telegram/bot/TelegramConstants.java
+++ src/main/java/org/telegram/bot/TelegramConstants.java
@@ -1,11 +1,7 @@
 package org.telegram.bot;
 
 public class TelegramConstants {
-    //    public static final String BOT_NAME = "JavaLessons";//"MyFirstGoItJava2021Bot";
-//
-//    public static final String BOT_TOKEN = "1891322670:AAEeHssBAywpVqKIaNJ7ZlHsIZ5BohhtJiQ";
-//    1891322670:AAEeHssBAywpVqKIaNJ7ZlHsIZ5BohhtJiQ
-    public static final String BOT_NAME = "MyFirstGoItJava2021Bot";
-    public static final String BOT_TOKEN = "1891322670:AAEeHssBAywpVqKIaNJ7ZlHsIZ5BohhtJiQ";
+    public static final String BOT_NAME = "";
+    public static final String BOT_TOKEN = "";
 
 }
